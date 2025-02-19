44c4114ec6c3ca4c110657f785bbebfad43f4abc
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-19 15:04:45 +0500

    wrote function that doubles a number

--- functions_task1.js
+++ functions_task1.js
@@ -1,8 +1,16 @@
 function getRandomNumberCallback (callback) {
     setTimeout (() => {
-        const randomNum = Math.floor(Math.random() * 100) + 1;
+        let randomNum = Math.floor(Math.random() * 100) + 1;
         console.log("Random number: ", randomNum);
         callback(randomNum);
-    }, 1000);
+    }, 3000);
 }
 
+function doubleNumberCallback (number) {
+    setTimeout (() => {
+        let doubledNum = number * 2;
+        console.log("Doubled number: ", doubledNum);
+    }, 2000);
+}
+
+getRandomNumberCallback(doubleNumberCallback);
