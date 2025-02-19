e82ef4e628038ca242cc11ddeb264394971e7a61
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-19 17:09:31 +0500

    added .then and .catch for promises and fixed resolve statements

--- functions_task3.js
+++ functions_task3.js
@@ -1,24 +1,29 @@
-function geRandomNumberPromise() {
-    return new Promise ((recolve, reject) => {
+function getRandomNumberPromise() {
+    return new Promise ((resolve, reject) => {
         setTimeout (() => {
-            let randomNum = Math.floor(Math.random() * 100) + 1;
+            let randomNum = Math.floor(Math.random() * 100);
             if (randomNum > 90) {
-                reject(new Error("Random number is greater than 90"));
+                reject(new Error("Rejected, random number is greater than 90"));
             } else {
-                resolve("Resolved, random number: ", randomNum);
+                resolve(randomNum);
             }
         }, 1000);
-    });
+    })
 }
 
 function doubleNumberPromise(num) {
     return new Promise ((resolve, reject) => {
         setTimeout (() => {
             if (num > 50) {
-                reject(new Error("Number is greater than 50"));
+                reject(new Error("Rejected, number is greater than 50"));
             } else {
-                num *= 2;
+                resolve(num * 2);
             }
         }, 500);
-    });
+    })
 }
+
+getRandomNumberPromise()
+.then((randomNum) => doubleNumberPromise(randomNum)) 
+.then((doubledNum) => console.log(`Result: ${doubledNum}`))
+.catch((err) => console.log(err))
