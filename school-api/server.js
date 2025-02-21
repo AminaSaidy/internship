0ff509d1febe5c4be0ca6196e80c886f6710adbd
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-21 11:14:15 +0500

    created class for school initialization

--- school-api/server.js
+++ school-api/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const one_page_size = 5;
+
+class School {
+    constructor(number, name, classesAmount, teachersAmount, status){
+        this.number = number;
+        this.name = name;
+        this.classesAmount = classesAmount;
+        this.teachersAmount = teachersAmount;
+        this.status = status;
+    }
+}
 
 app.get('/', (req, res) => {
     res.send("Hi");
