5b2d56cfe888fb55f68050418d42db204bcd388a
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-24 16:08:03 +0500

    made get request and async function and made it get info from database

--- school-api/server.js
+++ school-api/server.js
@@ -26,46 +26,54 @@ class School {
     }
 }
 
-function generateSchool(amount) {
-    let schools = [];
-    for(let i = 0; i < amount; i++) {
-        schools.push(
-            new School(
-                i, 
-                `State School #${i}`,
-                Math.floor(Math.random() * 30) + 10,
-                Math.floor(Math.random() * 40) + 20,
-                Math.random() < 0.5
-            )
-        );
-    }
-    return schools;
-}
+// function generateSchool(amount) {
+//     let schools = [];
+//     for(let i = 0; i < amount; i++) {
+//         schools.push(
+//             new School(
+//                 i, 
+//                 `State School #${i}`,
+//                 Math.floor(Math.random() * 30) + 10,
+//                 Math.floor(Math.random() * 40) + 20,
+//                 Math.random() < 0.5
+//             )
+//         );
+//     }
+//     return schools;
+// }
 
-let listOfSchools = generateSchool(25);
+// let listOfSchools = generateSchool(25);
 
-app.get('/api/school', (req, res) => {
+app.get('/api/school', async (req, res) => {
     //получаем номер страницы. по умолчанию 1, а если превышает колво страниц - 404 ошибка
     let page = parseInt(req.query.page) || 1;
     if(page < 1) page = 1;
     
-    let schoolsAmount = listOfSchools.length;
-    let pagesAmount = Math.ceil(schoolsAmount/pageSize);
+    try {
+        let countSchools = await pool.query("SELECT COUNT(*) FROM schools");
+        let schoolsAmount = parseInt(countSchools.rows[0].count);
+        let pagesAmount = Math.ceil(schoolsAmount/pageSize);
 
-    if(page > pagesAmount) {
-        return res.status(404).json({message: "Page not found"});
-    }
+        if(page > pagesAmount) {
+            return res.status(404).json({message: "Page not found"});
+        }
 
-    //индексы школ, которые будут выведены на конкретной странице
-    const startIndex = (page - 1) * pageSize;
-    let paginatedListSchools = listOfSchools.slice(startIndex, startIndex + pageSize);
+        //индексы школ, которые будут выведены на конкретной странице
+        let startIndex = (page - 1) * pageSize;
+        let result = await pool.query("SELECT * FROM schools ORDER BY id LIMIT $1 OFFSET $2",
+            [pageSize, startIndex]
+        );
 
-    res.json({
-        schools: paginatedListSchools,
-        currentPage: page,
-        schoolsAmount: schoolsAmount,
-        pagesAmount: pagesAmount
-    });
+        res.json({
+            schools: result.rows,
+            currentPage: page,
+            schoolsAmount,
+            pagesAmount
+        });
+    } catch (error) {
+    console.error(error);
+    res.status(500).json({message: "Database error"});
+}
 });
 
 app.post("/school", async (req, res) => {
