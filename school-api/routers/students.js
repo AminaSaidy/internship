8195e4d93f35af328e6c3b8772241aba13b6c7c0
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-25 18:20:22 +0500

    created post request function with check for class existance

--- school-api/routers/students.js
+++ school-api/routers/students.js
@@ -3,7 +3,31 @@ const router = express.Router();
 
 module.exports = (pool) => {
     router.post("/", async(req, res) => {
+        const {name, birth_date, gender, class_id, enrolled_at} = req.body;
 
+        if(!name || !birth_date || !gender || !class_id || !enrolled_at) {
+            return res.status(400).json({message: "Some required fields are empty"});
+        }
+
+        try {
+            let classCheck = await pool.query(
+                "SELECT id FROM classes WHERE id = $1",
+                [class_id]
+            );
+
+            if (classCheck.rows.length === 0) {
+                return res.status(400).json({message: "Class does not exist."});
+            }
+
+            let result = await pool.query(
+                "INSERT INTO students (name, birth_date, gender, class_id, enrolled_at) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+                [name, birth_date, gender, class_id, enrolled_at]
+            );
+            res.status(201).json(result.rows[0]);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({message: "Internal error."});
+        }
     });
 
     router.get("/", async(req, res) => {
