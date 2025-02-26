22336c456c893d29a3bd8818fdb10f3ef1f42df4
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-26 14:44:23 +0500

    created file for teachers api requests and wrote structure with router for it

---
+++ school-api/routers/teachers.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = (pool) => {
+    router.post("/", async (req, res) => {
+
+    });
+
+    router.get("/", async (req, res) => {
+
+    });
+
+    router.get("/:id", async (req, res) => {
+        
+    });
+    return router;
+}
