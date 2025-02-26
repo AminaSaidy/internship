dbce8f714f804de2fb3a96ac3390ffdd91a3be35
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-26 17:50:57 +0500

    wrote get request to get a list of teachers for a class

--- school-api/routers/classes.js
+++ school-api/routers/classes.js
@@ -84,5 +84,27 @@ module.exports = (pool) => {
             res.status(500).json({message: "Internal error."});
         }
     });
+
+    router.get("/:id/teachers", async (req, res) => {
+        let classId = parseInt(req.params.id);
+
+        if (isNaN(classId)) {
+            return res.status(400).json("Invalid class id");
+        }
+
+        try {
+            let result = await pool.query(
+                `SELECT t.id, t.name, t.birth_date, t.phone, t.email, t.hired_at 
+                FROM teachers t JOIN class_teachers ct ON t.id = ct.teacher_id 
+                WHERE ct.class_id = $1`,
+                [classId]
+            );
+
+            res.json(result.rows);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({message: "Internal error occured"});
+        }
+    });
     return router;
 }
