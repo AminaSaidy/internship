ca0209e18072bd4d3347819b1270f6c71053fbff
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-02-27 15:57:29 +0500

    deleted old irrelevant directories

--- chapter1/arrays_task4.js
+++
@@ -1,9 +0,0 @@
-let fruits = ['Banana', 'Apple', 'Orange', 'Mango', 'Cherry'];
-
-fruits.sort();
-console.log("Sorted array: ", fruits);
-
-let numbers = [8, 1, 13, 5, 0, 20];
-
-numbers.sort() //числа превращаются в строки и идет посимвольное сравнение слева направо по кодам UTF-16
-console.log("Sorted numbers: ", numbers);
--- chapter1/arrays_task7.js
+++
@@ -1,7 +0,0 @@
-let languages = ['JavaScript', 'Python', 'C++', 'Java', 'Ruby'];
-
-languages.splice(2, 1, 'C#', 'Go');
-console.log("Массив после замещения С++: ", languages);
-
-let cutElements = languages.splice(-2, 2);
-console.log("Вырезанные 2 последних элемента: ", cutElements);
