dc52893644db31c1ab16b1435638fce511953aa1
AminaSaidy <aminasaidakhmedova@gmail.com>
2025-03-07 12:34:28 +0500

    wrote all services and controlers functions and endpoints for subjects

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { SchoolsModule } from './schools/schools.module';
 import { StudentsModule } from './students/students.module';
 import { ClassesModule } from './classes/classes.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { SubjectsModule } from './subjects/subjects.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { ClassesModule } from './classes/classes.module';
     SchoolsModule,
     StudentsModule,
     ClassesModule,
+    TeachersModule,
+    SubjectsModule,
   ],
 })
 export class AppModule {}
